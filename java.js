9fbfc5732f6fd74c703b0d56ed4e79a413ef9d18
Rifkhi Akbar Putra Rustandi <91230120+rifkeh@users.noreply.github.com>
2022-07-18 01:17:20 +0700

    Update java.js

--- java.js
+++ java.js
@@ -20,6 +20,10 @@ function myFunction(){
         for(let i=0; i<line.length; i++){
             line[i].style.background="black";
         }
+        document.getElementById("right").style.background="white";
+        document.getElementById("readmore").style.background="white";
+        document.getElementById("readmore").style.color="black";
+        document.getElementById("readmore").style.border="1px solid black";
     }else{
         element.style.background="black";
         element.style.color="white";
@@ -35,8 +39,65 @@ function myFunction(){
         for(let i=0; i<line.length; i++){
             line[i].style.background="white";
         }
+        document.getElementById("right").style.background="black";
+        document.getElementById("readmore").style.background="black";
+        document.getElementById("readmore").style.color="white";
+        document.getElementById("readmore").style.border="1px solid white";
     }
     
     
 }
-document.body.style.background
+document.getElementById("readmore").onclick=function(){
+    location.href="#me"
+}document.getElementById("check").addEventListener("change", myFunction);
+function myFunction(){
+    var element=document.body;
+    var footer=document.getElementById("footer");
+    var afooter=footer.getElementsByTagName("a");
+    var hfooter=footer.getElementsByTagName("h4");
+    var line=document.getElementsByClassName("home-line");
+    if(element.style.background==="black"){
+        element.style.background="white";
+        element.style.color="black";
+        document.getElementById("menu").style.background="white";
+        document.getElementById("menu").style.boxShadow="0px 1px 3px black";
+        footer.style.boxShadow="0px 1px 3px black";
+        for(let i=0; i<afooter.length; i++){
+            afooter[i].style.color="black";
+        }
+        for(let i=0; i<hfooter.length; i++){
+            hfooter[i].style.color="black";
+        }
+        for(let i=0; i<line.length; i++){
+            line[i].style.background="black";
+        }
+        document.getElementById("right").style.background="white";
+        document.getElementById("readmore").style.background="white";
+        document.getElementById("readmore").style.color="black";
+        document.getElementById("readmore").style.border="1px solid black";
+    }else{
+        element.style.background="black";
+        element.style.color="white";
+        document.getElementById("menu").style.background="black";
+        document.getElementById("menu").style.boxShadow="0px 1px 2px white";
+        footer.style.boxShadow="0px 1px 3px white";
+        for(let i=0; i<afooter.length; i++){
+            afooter[i].style.color="white";
+        }
+        for(let i=0; i<hfooter.length; i++){
+            hfooter[i].style.color="white";
+        }
+        for(let i=0; i<line.length; i++){
+            line[i].style.background="white";
+        }
+        document.getElementById("right").style.background="black";
+        document.getElementById("readmore").style.background="black";
+        document.getElementById("readmore").style.color="white";
+        document.getElementById("readmore").style.border="1px solid white";
+    }
+    
+    
+}
+document.getElementById("readmore").onclick=function(){
+    location.href="#me"
+}
